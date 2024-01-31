f9bcf80e780b4cf2d0b9d564eae651d083a86722
Max Saparov <max.saparov@acronis.com>
2024-01-31 12:51:58 -0500

    improved prompt readability

--- src/openai.rs
+++ src/openai.rs
@@ -1,6 +1,45 @@
+use std::{fs, io::Read};
 use async_openai::{types::{ChatCompletionRequestAssistantMessageArgs, ChatCompletionRequestSystemMessageArgs, ChatCompletionRequestUserMessageArgs, CreateChatCompletionRequestArgs}, Client};
 use once_cell::sync::Lazy;
 use regex::Regex;
+
+// There may be a simpler way of creating these lazy strings (maybe caching?)
+fn get_system_prompt() -> Lazy<String> {
+    Lazy::new(|| {
+        let mut prompt = String::new();
+        let mut f = fs::File::open("prompts/system-prompt-1.txt")
+            .expect("i mean if this file is missing it's a big problem");
+        f.read_to_string(&mut prompt)
+            .expect("failing to read file is catastrophic");
+
+        prompt
+    })
+}
+
+fn get_user_prompt() -> Lazy<String> {
+    Lazy::new(|| {
+        let mut prompt = String::new();
+        let mut f = fs::File::open("prompts/user-prompt-1.txt")
+            .expect("i mean if this file is missing it's a big problem");
+        f.read_to_string(&mut prompt)
+            .expect("failing to read file is catastrophic");
+
+        prompt
+    })
+}
+
+fn get_assistant_prompt() -> Lazy<String> {
+    Lazy::new(|| {
+        let mut prompt = String::new();
+        let mut f = fs::File::open("prompts/assistant-prompt-1.txt")
+            .expect("i mean if this file is missing it's a big problem");
+        f.read_to_string(&mut prompt)
+            .expect("failing to read file is catastrophic");
+
+        prompt
+    })
+}
+
 
 pub async fn autofill_test(text: &String) -> Option<String>{
     static RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"<body.*?>").unwrap());
@@ -19,84 +58,17 @@ pub async fn autofill_test(text: &String) -> Option<String>{
         .model("gpt-4")
         .messages([
             ChatCompletionRequestSystemMessageArgs::default()
-                .content("You are a helpful program that simply parses html source code \
-                          and returns a single regex template that can be repeatedly used to pull out the title, a possible link, and possible \
-                          content from the repetitive site content.")
-                .build()
-                .unwrap()
-                .into(),
-            ChatCompletionRequestSystemMessageArgs::default()
-                .content(r##"However, use {*} as the wildcard instead of .*? and {%} as capture groups instead of (.*?). \
-                          Try to use at most only 5 {%} capture groups and simply discard unecessary information \
-                          such as style with {*}. For example,  <a style="font-size: 1.3rem;" href="https://blog.meain.io/2023/what-is-in-dot-git/" target="_blank"> \
-                          would become <a style="{*}" href="{%}" target="{*}">. \
-                          Simplicity is key here, get rid of as much hardcoded information with {*} as possible."##)
+                .content(get_system_prompt().to_string())
                 .build()
                 .unwrap()
                 .into(),
             ChatCompletionRequestUserMessageArgs::default()
-                .content(
-r##"
-<div class="tl-page">
-<header class="tl-main-header">
-<div class="tl-title">
-<h1>Plurrrr</h1>
-<span>home</span>
-</div>
-<nav>
-<ul class="tl-navigation">
-<li>
-<a href="/">home</a>
-</li>
-<li>
-<a href="/subscribe.html">subscribe</a>
-</li>
-<li>
-<a href="/tags/">tags</a>
-</li>
-<li>
-<a href="/about.html">about</a>
-</li>
-</ul>
-</nav>
-</header>
-<main>
-<time class="tl-date" datetime="2023-07-07">
-<a href="archive/2023/07/07.html" title="Nix shell template, IN vs. ANY, and Corinna in the Perl Core">Fri 07 Jul 2023</a>
-</time>
-<article>
-<h2 id="nix-shell-template">
-<a href="https://plurrrr.com/archive/2023/07/07.html#nix-shell-template">Nix shell template</a>
-</h2>
-<blockquote>
-<p>Nix shells are the best tool for creating software developmentenvironments right now. This article provides a template to get youstarted with Nix shells from scratch, and explains how to add commonfeatures.</p>
-</blockquote>
-<p>Source: <a href="https://paperless.blog/nix-shell-template">Nix shelltemplate</a>, an article byVictor Engmark.</p>
-<ul class="tl-tags">
-<li>
-<a href="https://plurrrr.com/tags/2023/nix.html">nix</a>
-</li>
-</ul>
-</article>
-<article>
-<h2 id="til---in-is-not-the-same-as-any">
-"##
-                )
+                .content(get_user_prompt().to_string())
                 .build()
                 .unwrap()
                 .into(),
             ChatCompletionRequestAssistantMessageArgs::default()
-                .content(r##"
-<article>
-<h2 id="{*}">
-<a href="{%}">{%}</a>
-</h2>
-<blockquote>
-<p>{%}</p>
-</blockquote>
-{*}
-</article>
-"##)
+                .content(get_assistant_prompt().to_string())
                 .build()
                 .unwrap()
                 .into(),
